23e475a35a8e351c1080c38223377df6957f2fc4
Ilaletdinov Almaz <45946541+blablatdinov@users.noreply.github.com>
2026-01-19 10:30:51 +0300

    Update license year (#78)

--- tests/it/test_app.py
+++ tests/it/test_app.py
@@ -1,24 +1,5 @@
-# The MIT License (MIT).
-#
-# Copyright (c) 2024-2025 Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
-# OR OTHER DEALINGS IN THE SOFTWARE.
+# SPDX-FileCopyrightText: Copyright (c) 2024-2026 Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
+# SPDX-License-Identifier: MIT
 
 # flake8: noqa: S . Not a production code
 
@@ -63,7 +44,7 @@ def test_dependency_versions(version: tuple[str]) -> None:
         check=False,
     )
 
-    assert got.stdout.decode('utf-8').strip() == 'file.py:36:5: OVR100 method must contain `typing.override` decorator'
+    assert got.stdout.decode('utf-8').strip() == 'file.py:17:5: OVR100 method must contain `typing.override` decorator'
     assert got.returncode == 1
 
 
@@ -78,5 +59,5 @@ def test() -> None:
 
     assert got.returncode == 1
     assert got.stdout.decode('utf-8').strip().splitlines() == [
-        'file.py:36:5: OVR100 method must contain `typing.override` decorator',
+        'file.py:17:5: OVR100 method must contain `typing.override` decorator',
     ]
